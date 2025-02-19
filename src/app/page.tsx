9b9c0d10440049f4e17638cfd9d27b157908377a
Hannah <hrose.mcmahon@gmail.com>
2025-02-19 12:42:34 -0700

    updated favicon and resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <Image
+          className="bg-opacity-0"
           src="/hannah_happy.png"
           alt="Hannah McMahon software developer in denver"
           width={180}
